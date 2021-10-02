6367ae1fdf7a141c5ea8a24e474ed0eb04af70a8
Grant Kee <grantkee@Grants-iMac.lan>
2021-10-02 17:04:56 -0500

    saving commented code for final version

---
+++ layers_demo.py
@@ -0,0 +1,26 @@
+import tensorflow as tf
+from tensorflow.keras import layers
+layer = layers.Dense(3) #3 neurons is the number we chose
+
+print(layer.weights) #we get empty weight and bias arrays because tensorflow doesn't know what the shape is of the input to this layer
+
+# 1338 samples, 11 features as in our dataset
+input = tf.ones((5000, 11)) 
+# a fully-connected layer with 3 neurons
+layer = layers.Dense(3) 
+# calculate the outputs
+output = layer(input) 
+# print the weights
+print(layer.weights) 
+
+print("\n\n\n")
+
+
+# 5000 samples, 11 features as in our dataset
+input = tf.ones((5000, 21)) 
+# a fully-connected layer with 3 neurons
+layer = layers.Dense(10) 
+# calculate the outputs
+output = layer(input) 
+# print the weights
+print(layer.weights) 
